5acfb94c27ed9ff8488ecc7a50eb6cc184b4e9e6
coderzzp <coderzzp@gmail.com>
2017-03-10 14:52:29 +0800

    修改细节

--- demo/表单验证/assets/login/js/index.js
+++ demo/表单验证/assets/login/js/index.js
@@ -1,5 +1,6 @@
 $(function(){
 	// $.fn.bootstrapValidator.extendRules({
+	//请注意：HTML5中的规则名称不能存在大写字母,所以不存在驼峰写法
 	// 	cardid : function(){
 	// 		return /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/.test(this.val());
 	// 	}
@@ -8,63 +9,15 @@ $(function(){
 	//用jquery不见得会用bs
 	$("#frmLogin").bootstrapValidator({
 		// triggerEvent : "change"
+		// errorMessage : "this field is required"
 	})
 	.on("error",function(event,$errFiles){
+		//$errFiles为一个存取错误信息的接口
 		// alert("验证有错误");
 		return false;
 	})
 	.on("success",function(event){
 		this.submit();
 		return false;
-	});//开启某表单的验证功能
-	//实现对登陆表单的验证
-	//用户名必须是字母数字下划线组成，不允许中文
-	//密码必须由字母数字组成，必须在8-12位之间
-	// $("#username").on("blur",function(){
-	// 	//\w正则里面意味着匹配一个字符（字母数字下划线）
-	// 	var $group = $(this).parents(".input-group").removeClass("has-success has-error");
-	// 	$group.next().remove();//移除之前的错误文本信息
-	// 	if(/^\w+$/.test(this.value)){
-	// 		$group.addClass("has-success");
-	// 	}else{
-	// 		$group.addClass("has-error");
-	// 		$group.after("<p class=\"text-danger\">输入不合法</p>");
-	// 	}
-	// });
-	// $("#password").on("blur",function(){
-	// 	//\w正则里面意味着匹配一个字符（字母数字下划线）
-	// 	var $group = $(this).parents(".input-group").removeClass("has-success has-error");
-	// 	$group.next().remove();//移除之前的错误文本信息
-	// 	if(/^[a-zA-Z0-9]{8,12}$/.test(this.value)){
-	// 		$group.addClass("has-success");
-	// 	}else{
-	// 		$group.addClass("has-error");
-	// 		$group.after("<p class=\"text-danger\">输入不合法</p>");
-	// 	}
-	// });
-	// $("#verifyCode").on("blur",function(){
-	// 	//\w正则里面意味着匹配一个字符（字母数字下划线）
-	// 	var $group = $(this).parents(".input-group").removeClass("has-success has-error");
-	// 	$group.next().remove();//移除之前的错误文本信息
-	// 	if(/^[a-zA-Z0-9]{4}$/.test(this.value)){
-	// 		$group.addClass("has-success");
-	// 	}else{
-	// 		$group.addClass("has-error");
-	// 		$group.after("<p class=\"text-danger\">输入不合法</p>");
-	// 	}
-	// });
-
-	// $("#frmLogin").on("submit",function(){
-	// 	var all1 = $(this).find("input").length;
-	// 	var all2 = $(this).find(".has-success").length;
-	// 	if(all1===all2){
-	// 		this.submit();
-	// 	}
-	// 	return false;
-	// 	// if($(this).find(".has-error").size()>0){
-	// 	// 	return false;
-	// 	// }else{
-	// 	// 	this.submit();
-	// 	// }
-	// });
+	});
 });
