9a358bcd48771c936edad2539f0ee84f618bf51a
斩玄 <zhanxuan@wacai.com>
2018-09-07 10:22:10 +0800

    faet:增加地址

---
+++ pwa/service-worker.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var cacheVersion = 0;
+var currentCache = {
+  offline: 'offline-cache' + cacheVersion
+};
+const offlineUrl = 'offline.html';
+
+this.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(currentCache.offline).then(function(cache) {
+      // 缓存
+      return cache.addAll([
+          './offline.svg',
+          offlineUrl
+      ]);
+    })
+  );
+});
+
+this.addEventListener('fetch', event => {
+  // 请求页面
+  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+      event.respondWith(
+        fetch(event.request.url).catch(error => {
+          // Return the offline page
+          return caches.match(offlineUrl);
+        })
+     );
+  }
+  // 请求图片
+  else{
+    event.respondWith(caches.match(event.request)
+        .then(function (response) {
+        return response || fetch('https://www.apiopen.top/satinApi?type=1&page=1').then(res=>{res.json().then(({data})=>console.log(data[0].text))})
+      })
+    );
+  }
+});
